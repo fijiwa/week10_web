75265fe39d41a01d1d2b7f4b40776f694e22f124
fijiwa <108624212+fijiwa@users.noreply.github.com>
2020-08-08 09:27:33 +0000

    MIPS: Loongson: Make platform serial setup always built-in.

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,33 @@
 
 class PlaylistsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @playlists = Playlist.all
+  end
+
+  def show
+    @playlist = Playlist.find(params[:id])
+  end
+
+  def new
+    @playlist = Playlist.new
+  end
+
+  def create
+    @playlist = Playlist.new(playlist_params)
+    if @playlist.save
+      redirect_to @playlist
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def playlist_params
+    params.require(:playlist).permit(:name, :number_of_votes)
+  end
+
+end
