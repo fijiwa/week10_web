e0a088ca5a1c7d6c2546e320c27acf24c079261b
fijiwa <108624212+fijiwa@users.noreply.github.com>
2021-12-23 21:02:21 +0000

    net-misc/youtube-dl: Version bump.

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,3 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
-require 'bundler/setup' # Set up gems listed in the G
+require 'bundler/setup' # Set up gems listed in the Gemfile.
