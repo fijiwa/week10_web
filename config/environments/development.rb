6877dc403430af488926c20828398509f8c7d834
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-04-17 22:51:38 +0000

    mm/hugetlb: check for pte NULL pointer in __page_check_address()

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,4 +19,5 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Raise an error on page load i
+  # Raise an error on page load if there are pending migrations.
+  config.active_r
