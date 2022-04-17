51b777c3e16365608ca43b5abd8f08d4bc0bf602
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-04-17 15:17:03 +0000

    Update seeds

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,4 +16,5 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  # Print deprecation notices to the Rails l
+  # Print deprecation notices to the Rails logger.
+  config.active_support.depre
