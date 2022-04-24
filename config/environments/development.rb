74a98c71745e4f457ce32a3442c064728c38adf2
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-04-24 14:21:29 +0000

    irda: Fix missing msg_namelen update in irda_recvmsg_dgram()

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,4 +20,6 @@ Rails.application.configure do
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
-  config.active_r
+  config.active_record.migration_error = :page_load
+
+  # Deb
