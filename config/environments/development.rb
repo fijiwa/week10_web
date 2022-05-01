620d1cf6990c46b2769a983c65e5ab06e6521015
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-05-01 10:29:40 +0000

    prctl: adds the capable(CAP_SYS_NICE) check to PR_SET_TIMERSLACK_PID.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,4 +24,7 @@ Rails.application.configure do
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
-  # number of complex ass
+  # number of complex assets.
+  config.assets.debug = true
+
+  # Asset diges
