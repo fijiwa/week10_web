c82c0a9d3c5d687c24f1370f43f8dc3603e0c42d
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-05-08 11:02:54 +0000

    Update project information

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,4 +32,5 @@ Rails.application.configure do
   config.assets.digest = true
 
   # Adds additional error checking when serving assets at runtime.
-  # Checks for improperly declared spro
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messa
