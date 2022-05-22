16708ff93616e56ccb256ed112205857b2e26b0c
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-05-22 05:30:55 +0000

    add demo gif

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,5 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  # Raises error for missin
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing
