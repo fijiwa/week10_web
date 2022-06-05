97488bac1c3e3e1d00e7bb85c5add2e5a1d353f7
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-06-05 15:05:23 +0000

    Delete me.lua

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,4 +5,4 @@ Rails.application.configure do
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
-  #
+  # your application in memory, allowing both threaded web s
