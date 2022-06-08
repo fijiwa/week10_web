8ec37c6a45f9534b7a73fab0913d33a9f85322db
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-06-08 07:44:16 +0000

    Update instructions.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,4 +6,5 @@ Rails.application.configure do
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to per
+  # and those relying on copy on write to perform better.
+  # Rake tasks automatically ignore th
