7e3b8b3e90dc871bbff9e70d6e46e8c073431c3b
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-07-17 09:16:39 +0000

    improved script

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,4 +17,5 @@ Rails.application.configure do
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
-  # NGINX, va
+  # NGINX, varnish or squid.
+  # config.action_dispatch.ra
