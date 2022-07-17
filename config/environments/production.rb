588875634cf483776fe0a41b301837e70f82ecec
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-07-17 08:56:12 +0000

    add new tests

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,4 +16,5 @@ Rails.application.configure do
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
-  # For large-scale production use, consider u
+  # For large-scale production use, consider using a caching reverse proxy like
+  # NGINX, va
