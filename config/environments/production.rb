b894effac49bc2b3f5ca93e65a8b347268fb851f
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-07-24 05:54:55 +0000

    Added some tests.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,4 +24,5 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
- 
+  # Compress JavaScripts and CSS.
+  config
