f4392180af6ebe5e7011415439d0102e81014175
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-07-28 05:24:04 +0000

    Fixed stuff.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,4 +32,5 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
-  # yet still be able to exp
+  # yet still be able to expire them through the digest params.
+  config.assets.diges
