7052f0dfc2ae7c7b85cbffc3d664c2d33ff67879
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-07-30 15:29:18 +0000

    update reame

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,3 +38,4 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
+  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for 
