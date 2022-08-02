57233334a00dab4eefe79b2eb73efceb5da9b8cf
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-08-02 07:26:20 +0000

    Delete .gitlab-ci.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,4 +39,6 @@ Rails.application.configure do
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
-  # config.action_dispatch.x_sendfile
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+
+  # Forc
