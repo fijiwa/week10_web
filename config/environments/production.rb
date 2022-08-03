a78a134e0ed19748a2f386cc7996ee3552d0a008
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-08-03 06:00:31 +0000

    Linux 3.10.47

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,4 +42,6 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # co
+  # config.force_ssl = true
+
+  # Use the lowest log level 
