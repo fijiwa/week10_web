1cb91a85405003c81dca7feb1ba68675455bbe1a
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-08-06 12:09:59 +0000

    copied from IAIS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,4 +44,4 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  # Use the lowest log level 
+  # Use the lowest log level to ensure availability of diag
