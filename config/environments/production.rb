9d47b9631c0617cb7894474b4b9bb1622e723d26
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-08-10 10:29:01 +0000

    oauth2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,4 +54,5 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
- 
+  # Use a different cache store in production.
+  # c
