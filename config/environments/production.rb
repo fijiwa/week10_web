a102b02ae1262a950b7dfb0a8f50c09be34722a8
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-08-12 11:57:30 +0000

    At work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,4 +58,6 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://as
+  # config.action_controller.asset_host = 'http://assets.example.com'
+
+  # Ignore bad email addresse
