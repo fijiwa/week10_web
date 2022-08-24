ca0f98afeedf333d50c04116a0de0d50b794c9d4
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-08-24 15:41:58 +0000

    lockd: protect nlm_blocked access in nlmsvc_retry_blocked

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,4 +62,6 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = f
+  # config.action_mailer.raise_delivery_errors = false
+
+  # Enable locale fallbacks for I18n (mak
