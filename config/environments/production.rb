4c1cb2d58ffa0c980f776b42665cb9146db4a31a
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-08-27 23:10:25 +0000

    7.0.0

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,4 +71,5 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  # Use default logging formatter so that PID and timestamp are not s
+  # Use default logging formatter so that PID and timestamp are not suppressed.
+  config.log_formatter = ::Logger:
