c24ae0e11418050ee987217cd11a5d7930c5090e
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-08-27 07:24:05 +0000

    &

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,4 +69,6 @@ Rails.application.configure do
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
-  config.active_support.depreca
+  config.active_support.deprecation = :notify
+
+  # Use default logging form
