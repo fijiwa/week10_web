0b9c594917d2e1fc2f4d0b39ff3d70e1a5c41ba0
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-09-05 07:34:30 +0000

    sched: Make sure to not re-read variables after validation

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.appl
+Rails.application.config.assets.version = '1.0'
+
+# A
