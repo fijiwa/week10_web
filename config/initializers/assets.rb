6f72c8a38f62450407899d7ac0eba68cb5daa2f1
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-09-07 19:00:55 +0000

    Improve README.md.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,4 +7,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already adde
