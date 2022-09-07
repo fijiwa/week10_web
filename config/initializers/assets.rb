0636e15b40bdf881a0bf1a5b479018015216630e
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-09-07 15:24:31 +0000

    1.6

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,4 +7,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
-# ap
+# application.js, application.css, and all non-JS
