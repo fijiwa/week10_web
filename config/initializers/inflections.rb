e206ec3db66d205ccf35b96a15c51fc69069df1e
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-09-28 14:43:00 +0000

    gitter

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -3,4 +3,5 @@
 # Add new inflection rules using the following format. Inflections
 # are locale specific, and you may define rules for as many different
 # locales as you wish. All of these examples are active by default:
-# ActiveSupport::Inflect
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflec
