e29df7bb71ad70f4afc1aef6d8fbbda7361157c3
fijiwa <108624212+fijiwa@users.noreply.github.com>
2022-09-30 12:41:46 +0000

    Changed the version number

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -6,4 +6,5 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
-#   inflect.irregular
+#   inflect.irregular 'person', 'people'
+#   inflect.uncountable %w( fi
