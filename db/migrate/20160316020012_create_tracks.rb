6774125ff25043c9be1ffeec4f65724189d89a6d
fijiwa <108624212+fijiwa@users.noreply.github.com>
2023-02-25 09:08:03 +0000

    fix deadlock

--- db/migrate/20160316020012_create_tracks.rb
+++ db/migrate/20160316020012_create_tracks.rb
@@ -2,4 +2,7 @@ class CreateTracks < ActiveRecord::Migration
   def change
     create_table :tracks do |t|
       t.string :title
-      
+      t.string :album
+      t.string :artist
+
+      t.times
